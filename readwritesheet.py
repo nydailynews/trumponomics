92257082d1852d03ca9b42037561096ca29a9ba4
freejoe76 <jmurphy@denverpost.com>
2017-05-02 15:22:39 -0400

    This file when run from the command line publishes the spreadsheet as json. It can also be included to help with the updates from the various APIs

---
+++ readwritesheet.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python
+# Read, write and publish the spreadsheets
+import sys
+import argparse
+import re
+import string
+import doctest
+import json
+from sheetmodule.writesheet import Sheet
+
+class EditSheet(Sheet):
+
+    def update(self, worksheet=None):
+        """ Publish the data in whatever permutations we need.
+            This assumes the spreadsheet's key names are in the first row.
+            >>> sheet = Sheet('test-sheet', 'worksheet-name')
+            >>> sheet.fix()
+            True
+            """
+        if not self.sheet or worksheet:
+            self.sheet = self.open_worksheet(worksheet)
+
+        if not worksheet:
+            worksheet = self.worksheet
+
+        cell_list = worksheet.get_all_values()
+        i = 0
+        for row in cell_list:
+            i += 1
+            print row
+            if row[0] == '':
+                value = url.replace('/', '-')
+                column = 1
+                worksheet.update_cell(i, column, value)
+
+        return True
+
+    def publish(self, worksheet=None):
+        """ Print out the json we'll use in different lists.
+            """
+        if not self.sheet or worksheet:
+            self.sheet = self.open_worksheet(worksheet)
+
+        if not worksheet:
+            worksheet = self.worksheet
+
+        cell_list = worksheet.get_all_values()
+        locations = {}
+        crimes = {}
+        recent = []
+        for i, row in enumerate(cell_list):
+            i += 1
+            if i == 1:
+                keys = row
+                continue
+            r = dict(zip(keys, row))
+            recent.append(r)
+
+        return {'recent': recent}
+
+def main(args):
+    """ main() only runs when this file's invoked from the command line, like so:
+        $ python readwritesheet.py labor-participation-rate monthly-job-creation
+        """
+    if args:
+        sheet = EditSheet('Trumponomics Dashboard')
+        sheet.set_options(args)
+        for worksheet in args.sheets[0]:
+            print worksheet
+            sheet.worksheet = sheet.open_worksheet(worksheet)
+            data = sheet.publish()
+            f = open('www/_output/%s.json' % worksheet, 'wb')
+            json.dump(data, f)
+            f.close()
+
+
+def build_parser(args):
+    """ This method allows us to test the args.
+        >>> args = build_parser(['--verbose'])
+        >>> print args.verbose
+        True
+        """
+    parser = argparse.ArgumentParser(usage='$ python readwritesheet.py',
+                                     description='Log in to Google sheets and do stuff with a spreadsheet and its tabs.',
+                                     epilog='Example use: python readwritesheet.py')
+    parser.add_argument("-v", "--verbose", dest="verbose", default=False, action="store_true")
+    parser.add_argument("sheets", action="append", nargs="*")
+    args = parser.parse_args(args)
+    return args
+
+if __name__ == '__main__':
+    args = build_parser(sys.argv[1:])
+
+    if args.verbose == True:
+        doctest.testmod(verbose=args.verbose)
+    main(args)
