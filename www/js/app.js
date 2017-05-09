227a740deeb768a6ff2d58f4c798b99c97d69df1
freejoe76 <jmurphy@denverpost.com>
2017-05-09 17:41:04 -0400

    Boilerplate js app

--- www/js/app.js
+++ www/js/app.js
@@ -1,1 +1,23 @@
-var dashboard = {}; 
+var dashboard = {
+    config: {},
+    update_config: function(config) {
+        // Take an external config object and update this config object.
+        for ( var key in config )
+        {
+            if ( config.hasOwnProperty(key) )
+            {
+                this.config[key] = config[key];
+            }
+        }
+    },
+    slugify: function (text) {
+        // from https://gist.github.com/mathewbyrne/1280286
+        return text.toString().toLowerCase()
+            .replace(/\s+/g, '-')           // Replace spaces with -
+            .replace(/[^\w\-]+/g, '')       // Remove all non-word chars
+            .replace(/\-\-+/g, '-')         // Replace multiple - with single -
+            .replace(/^-+/, '')             // Trim - from start of text
+            .replace(/-+$/, '');            // Trim - from end of text
+    },
+    init: function() {}
+}; 
