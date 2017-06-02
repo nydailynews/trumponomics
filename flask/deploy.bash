8e61085f9269424752e722bf55fff59d4603485e
freejoe76 <jmurphy@denverpost.com>
2017-06-02 13:35:43 -0400

    Deploy fixes

--- flask/deploy.bash
+++ flask/deploy.bash
@@ -3,8 +3,8 @@
 ENVIRON='PROD'
 export environ=$ENVIRON
 SERVER='qa'
-DIR='/apps/fiction/'
-PROJECT='coup'
+DIR='/apps/project/'
+PROJECT='trumponomics'
 LOCATION_OVERRIDE=0
 html_only() { 
     echo ''
